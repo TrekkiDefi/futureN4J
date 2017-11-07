d0e9fca8d6aa9ebc385cd7b26a1509d5d066a474
liuchunlong <631521383@qq.com>
2017-11-07 10:53:52 +0800

    Update build

--- futureN4J-common/src/main/java/com/github/ittalks/fn/common/advice/JsonpAdvice.java
+++ futureN4J-common/src/main/java/com/github/ittalks/fn/common/advice/JsonpAdvice.java
@@ -2,12 +2,14 @@ package com.github.ittalks.fn.common.advice;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 /**
  * Created by 刘春龙 on 2017/5/25.
- *
- * 用{@code RequestMappingHandlerAdapter}和{@code ExceptionHandlerExceptionResolver}注册，
- * 或者更倾向于用{@code @ControllerAdvice}注解，在这种情况下，JsonpAdvice将被两者自动检测
+ * <p>
+ * 用{@link RequestMappingHandlerAdapter}和{@link ExceptionHandlerExceptionResolver}注册，
+ * 或者更倾向于用{@link ControllerAdvice @ControllerAdvice}注解，在这种情况下，JsonpAdvice将被两者自动检测
  */
 @ControllerAdvice
 public class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
